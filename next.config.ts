4ecac93222290b1a7acceab45c6ffb2dbe41bd9b
zees123-svg <versatilezeeshan1708@gmail.com>
2026-04-20 10:10:07 -1100

    add changs in all files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+const nextConfig = {
+  output: 'export',  
+  images: {
+    unoptimized: true,
+  },
+}
+module.exports = nextConfig;
